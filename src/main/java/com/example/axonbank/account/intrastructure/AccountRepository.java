53e60c3ebcbe84dce0dbf26df81f937c1ce950d2
Liang Chen <liachen@thoughtworks.com>
2018-02-28 10:41:13 +0800

    add data rest support.

--- src/main/java/com/example/axonbank/account/intrastructure/AccountRepository.java
+++ src/main/java/com/example/axonbank/account/intrastructure/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.example.axonbank.account.intrastructure;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends CrudRepository<Account, String> {
+@RepositoryRestResource(path="accounts")
+public interface AccountRepository extends JpaRepository<Account, String> {
 }
